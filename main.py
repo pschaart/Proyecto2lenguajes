cf698737ffb53524b4aa0b8623026e4ba48d9d67
pschaart <pschaart@gmail.com>
2021-05-05 03:28:23 -0600

    completado

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def GenerarAutomata():
         #raise Exception()
         print('Ha ocurrido un error')
 
-def AnalizarCadena():
+def AnalizarCadenaG():
     try:
         f = open('archivo.html', 'w')
         mensaje = []
@@ -434,6 +434,184 @@ def AnalizarCadena():
     except:
         print('Ha ocurrido un error')
 
+def AnalizarCadenaT():
+    try:
+        f = open('archivoT.html', 'w')
+        iteracion = 0
+        mensaje = []
+        mensaje.append('<html>'
+                       '<head></head>')
+        mensaje.append(
+            '<table border="1">'
+            '<tr>'
+            '<td>iteracion</td>'
+            '<td>pila</td>'
+            '<td>entrada</td>'
+            '<td>transiciones</td>'
+            '</tr>')
+        pila = []
+        for i in range(len(ADPES)):
+            print(str(i + 1) + '.' + ADPES[i].Nombre)
+        print('seleccione una Automata de pila:')
+        ADPusar = ADPES[int(input()) - 1]
+        print('ingrese una cadena')
+        cadena = input()
+        pila.append('#')
+        iteracion += 1
+        mensaje.append(
+            '<tr>'
+            '<td>' + str(iteracion) + '</td>'
+            '<td>' + ''.join(pila) + '</td>'
+            '<td>' + cadena + '</td>'
+            '<td>(i, $, $;p,#)</td>'
+            '</tr>')
+        inicial = ADPusar.Estadop
+        pila.append(inicial)
+        iteracion += 1
+        mensaje.append(
+            '<tr>'
+            '<td>' + str(iteracion) + '</td>'
+            '<td>' + ''.join(pila) + '</td>'
+            '<td>' + cadena + '</td>'
+            '<td>(p, $, $;q,'+ inicial +')</td>'
+            '</tr>')
+        while cadena != '' or len(pila) != 1 :
+            iteracion += 1
+            print(pila)
+            ingresada = False
+            ultimo = pila[len(pila)-1]
+            transis = 0
+            for i in ADPusar.Estadoqar:
+                transi1 = i.split(';')
+                transi2 = transi1[0].split(',')
+                if transi2[1] == ultimo:
+                    transis += 1
+            for i in ADPusar.Estadoqar:
+                transi1 = i.split(';')
+                transi1[1] = ''.join(transi1[1].split(' '))
+                transi2 = transi1[0].split(',')
+                if transis == 1:
+                    pila.pop()
+                    num = len(transi1[1]) - 1
+                    for k in transi1[1]:
+                        pila.append(transi1[1][num])
+                        num -= 1
+                    ingresada = True
+                    mensaje.append(
+                        '<tr>'
+                        '<td>' + str(iteracion) + '</td>'
+                        '<td>' + ''.join(pila) + '</td>'
+                        '<td>' + cadena + '</td>'
+                        '<td>(q,$,'+ transi2[1]+';q,'+ ultimo +')</td>'
+                        '</tr>')
+                    break
+
+                elif transi2[1] == ultimo:
+                    if len(transi1[1]) > 1 and cadena[0] in transi1[1]:
+                        pila.pop()
+                        num = len(transi1[1]) - 1
+                        for k in transi1[1]:
+                            pila.append(transi1[1][num])
+                            num -= 1
+                        ingresada = True
+                        mensaje.append(
+                            '<tr>'
+                            '<td>' + str(iteracion) + '</td>'
+                            '<td>' + ''.join(pila) + '</td>'
+                            '<td>' + cadena + '</td>'
+                            '<td>(q,$,'+ transi2[1]+';q,'+ ultimo +')</td>'
+                            '</tr>')
+                        break
+                    elif cadena[0] in transi1[1]:
+                        pila.pop()
+                        num = len(transi1[1]) - 1
+                        for k in transi1[1]:
+                            pila.append(transi1[1][num])
+                            num -= 1
+                        ingresada = True
+                        mensaje.append(
+                            '<tr>'
+                            '<td>' + str(iteracion) + '</td>'
+                            '<td>' + ''.join(pila) + '</td>'
+                            '<td>' + cadena + '</td>'
+                            '<td>(q,$,'+ transi2[1]+';q,'+ ultimo +')</td>'
+                            '</tr>')
+                        break
+            if ingresada == False:
+                for i in ADPusar.Estadoqar:
+                    transi1 = i.split(';')
+                    transi1[1] = ''.join(transi1[1].split(' '))
+                    transi2 = transi1[0].split(',')
+                    if (transi2[1] == ultimo) and (len(transi1[1]) == 1):
+                        pila.pop()
+                        num = len(transi1[1]) - 1
+                        for k in transi1[1]:
+                            pila.append(transi1[1][num])
+                            num -= 1
+                        ingresada = True
+                        mensaje.append(
+                            '<tr>'
+                            '<td>' + str(iteracion) + '</td>'
+                            '<td>' + ''.join(pila) + '</td>'
+                            '<td>' + cadena + '</td>'
+                            '<td>(q,$,'+ transi2[1]+';q,'+ ultimo +')</td>'
+                            '</tr>')
+                        break
+            if ingresada == False:
+                for i in ADPusar.Estadoqab:
+                    transi1 = i.split(';')
+                    transi2 = transi1[0].split(',')
+                    if transi2[0] == ultimo:
+                        cadena = cadena.lstrip(ultimo)
+                        pila.pop()
+                        ingresada = True
+                        mensaje.append(
+                            '<tr>'
+                            '<td>' + str(iteracion) + '</td>'
+                            '<td>' + ''.join(pila) + '</td>'
+                            '<td>' + cadena + '</td>'
+                            '<td>(q,' + transi1[0] + ';q,' + ultimo + ')</td>'
+                            '</tr>')
+                        break
+            if ingresada == False:
+                mensaje.append(
+                    '<tr>'
+                    '<td>' + str(iteracion) + '</td>'
+                    '<td>' + ''.join(pila) + '</td>'
+                    '<td>' + cadena + '</td>'
+                    '<td> vacio </td>'
+                    '</tr>'
+                    '</table>'
+                    'Cadena rechazada')
+                print('Cadena rechazada')
+                mensaje.append('</html>')
+                mensaje = ''.join(mensaje)
+                f.write(mensaje)
+                f.close()
+                subprocess.Popen(['archivo.html'], shell=True)
+                break
+        print(pila)
+        if len(cadena) > 1:
+            return
+        else:
+            pila.pop()
+            iteracion += 1
+            mensaje.append(
+                '<tr>'
+                '<td>' + str(iteracion) + '</td>'
+                '<td>' + ''.join(pila) + '</td>'
+                '<td>' + cadena + '</td>'
+                '<td>(q,$,#;f,$)</td>'
+                '</tr>')
+        mensaje.append('</html>')
+        mensaje = ''.join(mensaje)
+        f.write(mensaje)
+        f.close()
+        subprocess.Popen(['archivoT.html'], shell=True)
+    except:
+        #raise Exception()
+        print('Ha ocurrido un error')
+
 #-----------Menu-----------
 segundos = 5
 print('----------Proyecto 2---------')
@@ -445,27 +623,33 @@ while segundos != 0:
     segundos -= 1
 opcion = 0
 print('!Bienvenido¡')
-while opcion != 6:
-    try:
-        print('------------Menu---------------')
-        print('1.Cargar el archivo')
-        print('2.Mostrar informacion general de la gramatica')
-        print('3.Generar automata de pila eqivalente')
-        print('4.Reporte de recorrido')
-        print('5.Reporte en tabla')
-        print('6.Salir')
-        opcion = input()
-        if int(opcion) == 1:
-            root = Tk()
-            NombreArchivo = askopenfilename()
-            root.withdraw()
-            Leer(NombreArchivo)
-        elif int(opcion) == 2:
-            MostrarGramaticas()
-        elif int(opcion) == 3:
-            GenerarAutomata()
-        elif int(opcion) == 4:
-            AnalizarCadena()
-    except:
-        #raise Exception()
-        print('ha ocurrido un error... regresando al menu principal')
+try:
+    while int(opcion) != 6:
+        try:
+            print('------------Menu---------------')
+            print('1.Cargar el archivo')
+            print('2.Mostrar informacion general de la gramatica')
+            print('3.Generar automata de pila eqivalente')
+            print('4.Reporte de recorrido')
+            print('5.Reporte en tabla')
+            print('6.Salir')
+            opcion = input()
+            if int(opcion) == 1:
+                root = Tk()
+                NombreArchivo = askopenfilename()
+                root.withdraw()
+                Leer(NombreArchivo)
+            elif int(opcion) == 2:
+                MostrarGramaticas()
+            elif int(opcion) == 3:
+                GenerarAutomata()
+            elif int(opcion) == 4:
+                AnalizarCadenaG()
+            elif int(opcion) == 5:
+                AnalizarCadenaT()
+        except:
+            #raise Exception()
+            print('ha ocurrido un error... regresando al menu principal')
+except:
+    #raise Exception()
+    print('ha ocurrido un error...')
